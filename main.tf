0edbb2be7389ea4e20e4734fa7b7bae28c2be24c
mamatha-ravi <mamata.r@gmail.com>
2026-03-03 20:56:52 +0530

    sg

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "main" {
+  name        = "${var.project}-${var.environment}-${var.component}"
+  description = "Allow TLS inbound traffic for ${var.project} in ${var.environment} for component ${var.component}"
+  vpc_id      = var.vpc_id
+ egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags = merge(
+     var.sg_tags,
+    local.common_tags,
+  {
+        Name = "${var.project}-${var.environment}-${var.component}"
+    }
+  )
+}
